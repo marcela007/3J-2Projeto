37036dd72ade2d98a31928b5968ad1de6869f12e
marcela007 <110061268+marcela007@users.noreply.github.com>
2024-10-18 09:31:54 -0300

    Delete quantidadeDeUsuarios.js

--- quantidadeDeUsuarios.js
+++
@@ -1,7 +0,0 @@
-async function quantidadeDeUsuarios(){
-const url= 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-const resultado = await fetch(url);
-const dados = await resultado.json();
-const nomeDasRedes = Object.keys(dados);
-const quantidadeDeUsuarios = Object.values(dados);
-}
